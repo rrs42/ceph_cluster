a96a4a95dc8626e7e602cb692608a30b88ad78a6
Russell Steffen <rsteffen@gmail.com>
2018-09-28 17:17:32 -0700

    simple python aid for listing ansible group member

---
+++ aws_ceph_util.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import subprocess
+import argparse
+
+
+def get_ansible_inventory():
+    output = subprocess.check_output(['ansible-inventory', '--list', 'ceph'])
+
+    s = output.decode("utf-8")
+    return json.loads(str(s))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--space', action='store_true')
+    parser.add_argument('-n', '--newline', action='store_true')
+
+    args, groups = parser.parse_known_args()
+
+    inv = get_ansible_inventory()
+
+    for g in groups:
+        hosts = inv[g]["hosts"]
+        if args.space:
+            result = " ".join(hosts)
+        elif args.newline:
+            result = "\n".join(hosts)
+        else:
+            result = ",".join(hosts)
+
+        print(result)
